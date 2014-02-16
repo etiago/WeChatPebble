459c3f0ce8fd5b7c972d9b40a6411af778e5214d
Weber <weberwpchu@gmail.com>
2014-02-16 15:37:58 +0800

    Fix indentation

--- wechatpebble.c
+++ wechatpebble.c
@@ -17,76 +17,76 @@ static int offset = 0;
 
 
 void in_received(DictionaryIterator *received, void *context) {
-        app_comm_set_sniff_interval(SNIFF_INTERVAL_REDUCED);
-        app_comm_set_sniff_interval(SNIFF_INTERVAL_NORMAL);
-        
-        Tuple* message = dict_find(received, 1);
-        
-        Tuple* reset = dict_find(received, 2);
-        
-        Tuple* finished = dict_find(received, 3);
-        
-        if (reset != NULL) {
-                offset = 0;
-                memset(character_data, 0xff, 3200);
-        }
-        
-        if (finished != NULL) {
-                layer_mark_dirty(bitmap_layer_get_layer(character_layer));        
-                vibes_short_pulse();
-                return;
-        }
-        
-        //size_t offset = i * 2;
-        
-        if (message != NULL) {
-                memcpy(&character_data[offset], message->value->data, message->length);
-                offset+=BITMAP_WIDTH;
-        }
-        
-        /*Tuplet value = TupletInteger(1, message->value->data[0]);
-        DictionaryIterator *iter;
-        app_message_out_get(&iter);
-        
-        if (iter == NULL) {
-                return;
-        }
-        
-        dict_write_tuplet(iter, &value);
-        dict_write_end(iter);
-        
-        app_message_out_send();
-        app_message_out_release();*/
-        //memcpy(character_data + offset, message->value->data + 1, message->length - 1);
+    app_comm_set_sniff_interval(SNIFF_INTERVAL_REDUCED);
+    app_comm_set_sniff_interval(SNIFF_INTERVAL_NORMAL);
+
+    Tuple* message = dict_find(received, 1);
+
+    Tuple* reset = dict_find(received, 2);
+
+    Tuple* finished = dict_find(received, 3);
+
+    if (reset != NULL) {
+        offset = 0;
+        memset(character_data, 0xff, 3200);
+    }
+
+    if (finished != NULL) {
+        layer_mark_dirty(bitmap_layer_get_layer(character_layer));
+        vibes_short_pulse();
+        return;
+    }
+
+    //size_t offset = i * 2;
+
+    if (message != NULL) {
+        memcpy(&character_data[offset], message->value->data, message->length);
+        offset+=BITMAP_WIDTH;
+    }
+
+    /*Tuplet value = TupletInteger(1, message->value->data[0]);
+    DictionaryIterator *iter;
+    app_message_out_get(&iter);
+
+    if (iter == NULL) {
+        return;
+    }
+
+    dict_write_tuplet(iter, &value);
+    dict_write_end(iter);
+
+    app_message_out_send();
+    app_message_out_release();*/
+    //memcpy(character_data + offset, message->value->data + 1, message->length - 1);
    //text_layer_set_text(&hello_layer, message->value->cstring);
 }
 
 void in_dropped(AppMessageResult reason, void *context) {
-        /*if (reason == APP_MSG_CALLBACK_NOT_REGISTERED) {
-                text_layer_set_text(&hello_layer, "Hello not registered!");
-        } else if (reason == APP_MSG_CALLBACK_ALREADY_REGISTERED) {
-                text_layer_set_text(&hello_layer, "Hello already registered!");
-        } else if (reason == APP_MSG_ALREADY_RELEASED) {
-                text_layer_set_text(&hello_layer, "Hello already released!");
-        }else if (reason == APP_MSG_BUFFER_OVERFLOW) {
-                text_layer_set_text(&hello_layer, "Hello buff over!");
-        }else if (reason == APP_MSG_BUSY) {
-                text_layer_set_text(&hello_layer, "Hello busy!");
-        }else if (reason == APP_MSG_INVALID_ARGS) {
-                text_layer_set_text(&hello_layer, "Hello invalid args!");
-        }else if (reason == APP_MSG_APP_NOT_RUNNING) {
-                text_layer_set_text(&hello_layer, "Hello not running!");
-        }else if (reason == APP_MSG_OK) {
-                text_layer_set_text(&hello_layer, "Hello ok!");
-        }else if (reason == APP_MSG_NOT_CONNECTED) {
-                text_layer_set_text(&hello_layer, "Hello not connected!");
-        }else if (reason == APP_MSG_SEND_REJECTED) {
-                text_layer_set_text(&hello_layer, "Hello send rejected!");
-        }else if (reason == APP_MSG_SEND_TIMEOUT) {
-                text_layer_set_text(&hello_layer, "Hello send timeout!");
-        }else {
-                text_layer_set_text(&hello_layer, "Hello Dropped!");
-        }*/
+    /*if (reason == APP_MSG_CALLBACK_NOT_REGISTERED) {
+        text_layer_set_text(&hello_layer, "Hello not registered!");
+    } else if (reason == APP_MSG_CALLBACK_ALREADY_REGISTERED) {
+        text_layer_set_text(&hello_layer, "Hello already registered!");
+    } else if (reason == APP_MSG_ALREADY_RELEASED) {
+        text_layer_set_text(&hello_layer, "Hello already released!");
+    }else if (reason == APP_MSG_BUFFER_OVERFLOW) {
+        text_layer_set_text(&hello_layer, "Hello buff over!");
+    }else if (reason == APP_MSG_BUSY) {
+        text_layer_set_text(&hello_layer, "Hello busy!");
+    }else if (reason == APP_MSG_INVALID_ARGS) {
+        text_layer_set_text(&hello_layer, "Hello invalid args!");
+    }else if (reason == APP_MSG_APP_NOT_RUNNING) {
+        text_layer_set_text(&hello_layer, "Hello not running!");
+    }else if (reason == APP_MSG_OK) {
+        text_layer_set_text(&hello_layer, "Hello ok!");
+    }else if (reason == APP_MSG_NOT_CONNECTED) {
+        text_layer_set_text(&hello_layer, "Hello not connected!");
+    }else if (reason == APP_MSG_SEND_REJECTED) {
+        text_layer_set_text(&hello_layer, "Hello send rejected!");
+    }else if (reason == APP_MSG_SEND_TIMEOUT) {
+        text_layer_set_text(&hello_layer, "Hello send timeout!");
+    }else {
+        text_layer_set_text(&hello_layer, "Hello Dropped!");
+    }*/
 }
 
 static void handle_init() {
@@ -94,17 +94,17 @@ static void handle_init() {
     window_stack_push(window, true /* Animated */);
 
   /*text_layer_init(&hello_layer, GRect(0, 65, 144, 30));
-text_layer_set_text_alignment(&hello_layer, GTextAlignmentCenter);
-text_layer_set_text(&hello_layer, "Hello World!");
-text_layer_set_font(&hello_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-layer_add_child(&window.layer, &hello_layer.layer);
+    text_layer_set_text_alignment(&hello_layer, GTextAlignmentCenter);
+    text_layer_set_text(&hello_layer, "Hello World!");
+    text_layer_set_font(&hello_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+    layer_add_child(&window.layer, &hello_layer.layer);
 
-        text_layer_set_text(&hello_layer, "Hello New!");*/
+    text_layer_set_text(&hello_layer, "Hello New!");*/
                                                                                          
         
     memset(character_data, 0xff, 3200);
         
-        // Album art
+    // Album art
     character_bitmap = (GBitmap) {
         .addr = character_data,
         .bounds = GRect(0, 0, 144, 160),
@@ -132,7 +132,7 @@ static void handle_deinit(void) {
 }
 
 int main(void) {
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+	handle_init();
+	app_event_loop();
+	handle_deinit();
 }
